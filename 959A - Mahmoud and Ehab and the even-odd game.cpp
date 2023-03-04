1e1b379b78fd79c170946c97793898cccfe520e9
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-03-04 14:49:13 +0600

    Add files via upload

---
+++ 959A - Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    if (x>=1){
+        if(x%2==0){cout<<"Mahmoud";}
+        else{cout<<"Ehab";}
+    }
+    return 0;
+}
