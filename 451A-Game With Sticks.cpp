285a7c1036388204461c644902b2ee470f417e43
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-03-10 04:52:23 +0600

    Add files via upload

---
+++ 451A-Game With Sticks.cpp
@@ -0,0 +1,24 @@
+/*
+#include<BISMILLAHIR RAHMANIR RAHIM>
+#include<Vladimir Putin>
+#define Putin_still_Alive 1/0
+pip install Putin_er_Khel
+Loading..############################## 99%
+Download Successfully ...
+//"Let's Rock & Roll ;)  "
+*/
+
+#include<bits/stdc++.h>
+#include<cmath>
+using namespace std;
+int main(){
+    int n,m,i=1,jotogula=0;cin>>n>>m;
+    for(;;){
+        if(n-i==0){break;}
+    else if(m-i==0){break;}
+    i+=1;
+    jotogula+=1;}
+    if(jotogula%2==0){cout<<"Akshat";}
+    else{cout<<"Malvika";}
+return 0;
+}
---
+++ 451A-Game with Sticks.py
@@ -0,0 +1,24 @@
+#######<786>#######
+#######[_PUTIN Still Alive_]######
+######Let's rock and Roolllll#####
+'''
+Loading Putin_er_Khel...99%################
+'''
+
+
+
+
+n,m=map(int,input().split())
+i=1
+koida=0
+for _ in range(max(n,m)):
+    if (n-i==0):
+        break
+    elif (m-i==0):
+        break
+    i+=1
+    koida+=1
+if(koida%2==0):
+    print("Akshat")
+else:
+    print("Malvika")
