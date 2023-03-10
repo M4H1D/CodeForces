4b00f3f929cbeb2b4252269af505672348cbe70f
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-03-11 02:44:35 +0600

    Add files via upload

---
+++ 58A-Chat room.py
@@ -0,0 +1,16 @@
+#######<786>#######
+#######[_PUTIN Still Alive_]######
+######Let's start rock and Roolllll#####
+from re import search as Vikings_Putin
+'''
+sudo upgrade && update
+pip install PUTIN
+Loading PUTIN...################### 99% done
+Download Successfully...
+from PUTIN import Putin_er_Khel as Conquer
+'''
+russia=input()
+if Vikings_Putin("h.*e.*l.*l.*o",russia):
+    print("YES")
+else:
+    print("NO")
