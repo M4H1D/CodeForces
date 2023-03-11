a5c1ed99a66b837256b698bb6238d47a0ff481c5
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-03-11 11:19:19 +0600

    Add files via upload

---
+++ 32B-Borze.py
@@ -0,0 +1,15 @@
+#######<786>#######
+#######[_PUTIN Still Alive_]######
+######Let's start rock and Roolllll#####
+from re import search as Vikings_Putin
+'''
+sudo upgrade && update
+pip install PUTIN
+Loading PUTIN...################### 99% done
+Download Successfully...
+from PUTIN import Putin_er_Khel as Conquer
+'''
+R=str.replace;print(R(R(R(input(),'--','2'),'-.','1'),'.','0'))
+"""
+___Vladimir Putin_____
+"""
