fe626cfc3bbdcbf424c402b4f89153aac86aaa21
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-03-11 18:02:21 +0600

    Add files via upload

---
+++ 472A-Design Tutorial learn from math.cpp
@@ -0,0 +1,29 @@
+/*
+#include<BISMILLAHIR RAHMANIR RAHIM>
+#include<Vladimir Putin>
+#define Putin_still_Alive 1/0
+pip install Putin_er_Khel
+Loading..############################## 99%
+Download Successfully ...
+//"Let's Rock & Roll ;)  "
+*/
+#include<iostream>
+#include<bits/stdc++.h>
+#include<cmath>
+#include<algorithm>
+#include<string>
+#define FOUR 4
+#define NINE 9
+using namespace std;
+int main(){
+    int n;cin>>n;
+    if(n%2==0){cout<<FOUR<<" "<<n-FOUR;}
+    else{cout<<abs(n-NINE)<<" "<<NINE;}
+    return 0;
+}
+/*
+Even death, Putin is The Hero
+while(true){
+    printf("I am the fan of PUTIN\n");
+}
+*/
