b7820e60667e151a10de9f81ed2de95ad578578d
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-03-13 02:39:44 +0600

    Add files via upload

---
+++ 427A-Police Recruits.py
@@ -0,0 +1,95 @@
+#######<786>#######
+#######[_PUTIN Still Alive_]######
+######Let's start rock and Roolllll#####
+n=int(input())####
+########
+li=list(map(int,input().split()))[:n]
+'''
+sudo upgrade && update
+pip install PUTIN
+Loading PUTIN...################### 99% done...
+Download Successfully...
+from PUTIN import Putin_er_Khel as Conquer
+'''
+s=0
+k=0
+"""
+Even death, Putin is The Hero
+while(true){
+    printf("I am the fan of PUTIN\n");
+}
+
+
+"""
+
+
+
+
+
+
+#// When I wrote this, only ALLAH and I understood what I was doing.
+# Now, ALLAH only knows.
+
+#/* You are not meant to understand this */
+
+
+
+for i in li:
+    s+=i
+    if s<0:
+        k+=i;s=0
+
+
+
+"""
+while(1>0)
+    {
+        1.Wait here until Vikings PUTIN strike again.
+        2.PUTIN always support us..
+    }
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+created by  
+Mahid
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(abs(k))
